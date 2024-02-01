9d6ad26636a000a30efe7755e8f90c569de270e7
Timur <140169369+VetrovTimur@users.noreply.github.com>
2024-02-01 08:57:29 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import defaultGame, { GameSavingData, loadGame as loadGame , saveGame as saveGame } from "./game.js";
+import Game, { GameSavingData, loadGame, saveGame } from "./game.js";
 
 const game = new Game();
 game.start();
